c40568a2ff80c6863bb4b7754fe3b7db4b29c4d9
rei <rei@AMS-GYM-EDV1-01.gym.ams.local>
2019-03-04 14:52:38 +0100

    QuickSort Rahmen

--- HalloWeltProjekt/src/sort/QuickSort.java
+++ HalloWeltProjekt/src/sort/QuickSort.java
@@ -11,11 +11,34 @@ public class QuickSort {
 	}
 
 	public static void quicksort(int links, int rechts) {
+		// abbruchbedingung ( bedingung
+		// teiler = teile
+		// 2x quicksort
 
 	}
 
 	public static int teile(int links, int rechts) {
+		int i = 0; // geändert!!
+		int j = 0; // auch hier
 		//
+		do {
+			// Suche von links ein Element, welches größer als das Pivotelement ist
+			while (i < rechts -1 && zahlen[XYZ] < pivot) {
+				// code
+			}
+			//
+			while (j < links && zahlen[XYZ] >= pivot) {
+				// code
+			}
+			//
+			if (i < j) {
+				// tausche
+			}
+		} while (i < j);
+		//
+		if (zahlen[XYZ] > pivot) {
+          //tausche daten[i] mit pivot
+		}
 		//
 		return i;
 	}
